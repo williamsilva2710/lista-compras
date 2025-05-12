271dd4c5d9c53b86f6dd67018c5029061f018817
williamsilva2710 <160758550+williamsilva2710@users.noreply.github.com>
2025-05-12 12:16:23 -0300

    gewg

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
+  'https://uxwjdjfqhziymxlcwcrc.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4d2pkamZxaHppeW14bGN3Y3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1MjgsImV4cCI6MjA2MjExMTUyOH0.1BgZsOjt4Iz9jbF5D5K1bGX8f9mVn9heWVNLb1oFZHk'
 )
