0e8ae1a9dccb3fdfec520c2f3831565815e0d3bc
williamsilva2710 <160758550+williamsilva2710@users.noreply.github.com>
2025-05-13 12:19:23 -0300

    dqw

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function carregarLista() {
   lista.innerHTML = ''
   data.forEach((item) => {
     const li = document.createElement('li')
-    li.innerHTML = `${item.item} <button onclick="removerItem('${item.id}')" style="border: 1px solid black; background-color: #fff; cursor:pointer">X</button>`
+    li.innerHTML = `${item.item} <button onclick="removerItem('${item.id}')" style="background-color: #fff; cursor:pointer">X</button>`
     lista.appendChild(li)
   })
 }
